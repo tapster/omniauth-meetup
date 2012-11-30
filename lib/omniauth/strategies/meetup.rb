f9a62e329118ea2c738bab8dcb68f6f8bc3709b4
Tristan Kromer <accounts@tristankromer.com>
2012-11-30 08:19:43 -0800

    Update lib/omniauth/strategies/meetup.rb
    Clean up with suggestion from @tapster.

--- lib/omniauth/strategies/meetup.rb
+++ lib/omniauth/strategies/meetup.rb
@@ -23,8 +23,8 @@ module OmniAuth
           :name => raw_info['name'],
           :photo_url => (raw_info['photo'].present? ? raw_info['photo']['photo_link'] : nil),
           :urls => { 'public_profile' => raw_info['link'] },
-          :description => (raw_info['bio']),
-          :location => (raw_info['city'] + "#{ if !raw_info['state'].blank? then ', ' + raw_info['state'] end }, " + raw_info['country'])
+          :description => raw_info['bio'],
+          :location => raw_info.slice('city', 'state', 'country').values.reject(&:blank?).join(', ')
         }
       end
 
