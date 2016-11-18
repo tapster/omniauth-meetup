348eaa90a137056f26b3ebae5a2d78082f1b73b7
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-18 14:37:26 +0000

    Fix oauth2 regression for callback url mismatch

--- lib/omniauth/strategies/meetup.rb
+++ lib/omniauth/strategies/meetup.rb
@@ -33,6 +33,11 @@ module OmniAuth
         { 'raw_info' => raw_info }
       end
 
+      def callback_url
+        # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
+        options[:callback_url] || (full_host + script_name + callback_path)
+      end
+
       def raw_info
         @raw_info ||= JSON.parse(access_token.get('/2/member/self').body)
       end
