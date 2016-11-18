1e27e27cb96dcbf7d938eba60558acfd1fdf5d1c
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-18 14:44:40 +0000

    Fix quotes and space

--- spec/omniauth/strategies/meetup_spec.rb
+++ spec/omniauth/strategies/meetup_spec.rb
@@ -7,11 +7,11 @@ describe OmniAuth::Strategies::Meetup do
 
   describe '#client' do
     it 'should have the correct meetup site' do
-      subject.client.site.should eq("https://api.meetup.com")
+      subject.client.site.should eq('https://api.meetup.com')
     end
 
     it 'should have the correct authorization url' do
-      subject.client.options[:authorize_url].should eq("https://secure.meetup.com/oauth2/authorize")
+      subject.client.options[:authorize_url].should eq('https://secure.meetup.com/oauth2/authorize')
     end
 
     it 'should have the correct token url' do
@@ -34,40 +34,40 @@ describe OmniAuth::Strategies::Meetup do
 
   describe '#info' do
     it 'returns the name from raw_info' do
-      subject.stub(:raw_info) { { 'name' => 'Bert' }}
+      subject.stub(:raw_info) { { 'name' => 'Bert' } }
       subject.info[:name].should == 'Bert'
     end
 
     it 'returns the photo_url from raw_info if available' do
-      subject.stub(:raw_info) { { 'photo' => { 'photo_link' => 'http://meetup.com/bert.jpg' } }}
+      subject.stub(:raw_info) { { 'photo' => { 'photo_link' => 'http://meetup.com/bert.jpg' } } }
       subject.info[:photo_url].should == 'http://meetup.com/bert.jpg'
-      subject.stub(:raw_info) {{}}
-      subject.info[:photo_url].should == nil
+      subject.stub(:raw_info) { {} }
+      subject.info[:photo_url].should.nil?
     end
 
     it 'returns the image from raw_info if available' do
-      subject.stub(:raw_info) { { 'photo' => { 'photo_link' => 'http://meetup.com/bert.jpg' } }}
+      subject.stub(:raw_info) { { 'photo' => { 'photo_link' => 'http://meetup.com/bert.jpg' } } }
       subject.info[:image].should == 'http://meetup.com/bert.jpg'
-      subject.stub(:raw_info) {{}}
-      subject.info[:image].should == nil
+      subject.stub(:raw_info) { {} }
+      subject.info[:image].should.nil?
     end
-    
+
     it 'returns the public_profile url' do
-      subject.stub(:raw_info) { { 'link' => 'http://meetup.com/bert' }}
+      subject.stub(:raw_info) { { 'link' => 'http://meetup.com/bert' } }
       subject.info[:urls][:public_profile].should == 'http://meetup.com/bert'
     end
 
     it 'returns the description from raw_info' do
-      subject.stub(:raw_info) { { 'bio' => 'My name is Bert.' }}
+      subject.stub(:raw_info) { { 'bio' => 'My name is Bert.' } }
       subject.info[:description].should == 'My name is Bert.'
     end
 
     it 'returns the location from raw_info' do
-      subject.stub(:raw_info) { { 'city' => 'Los Angeles', 'state' => 'CA', 'country' => 'USA' }}
+      subject.stub(:raw_info) { { 'city' => 'Los Angeles', 'state' => 'CA', 'country' => 'USA' } }
       subject.info[:location].should == 'Los Angeles, CA, USA'
-      subject.stub(:raw_info) { { 'city' => 'Tokyo', 'country' => 'Japan' }}
+      subject.stub(:raw_info) { { 'city' => 'Tokyo', 'country' => 'Japan' } }
       subject.info[:location].should == 'Tokyo, Japan'
-      subject.stub(:raw_info) {{}}
+      subject.stub(:raw_info) { {} }
       subject.info[:location].should == ''
     end
   end
