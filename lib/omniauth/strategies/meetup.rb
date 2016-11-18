ac5ad5766059f162892f77a2d38c26cce1102cb6
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-18 14:37:00 +0000

    Cleanup syntax

--- lib/omniauth/strategies/meetup.rb
+++ lib/omniauth/strategies/meetup.rb
@@ -2,30 +2,30 @@ require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
+    # Meetup omniauth-oauth2 strategy
     class Meetup < OmniAuth::Strategies::OAuth2
-      option :name, "meetup"
+      option :name, 'meetup'
 
-      option :client_options, {
-        :site => "https://api.meetup.com",
-        :authorize_url => 'https://secure.meetup.com/oauth2/authorize',
-        :token_url => 'https://secure.meetup.com/oauth2/access'
-      }
+      option :client_options,
+             site: 'https://api.meetup.com',
+             authorize_url: 'https://secure.meetup.com/oauth2/authorize',
+             token_url: 'https://secure.meetup.com/oauth2/access'
 
       def request_phase
         super
       end
 
-      uid{ raw_info['id'] }
+      uid { raw_info['id'] }
 
       info do
         {
-          :id => raw_info['id'],
-          :name => raw_info['name'],
-          :photo_url => (raw_info.key?('photo') ? raw_info['photo']['photo_link'] : nil),
-          :image => (raw_info.key?('photo') ? raw_info['photo']['photo_link'] : nil),
-          :urls => { :public_profile => raw_info['link'] },
-          :description => raw_info['bio'],
-          :location => [raw_info['city'], raw_info['state'], raw_info['country']].reject{|v| !v || v.empty?}.join(', ')
+          id: raw_info['id'],
+          name: raw_info['name'],
+          photo_url: meetup_photo_url,
+          image: meetup_photo_url,
+          urls: { public_profile: raw_info['link'] },
+          description: raw_info['bio'],
+          location: meetup_location
         }
       end
 
@@ -37,6 +37,17 @@ module OmniAuth
         @raw_info ||= JSON.parse(access_token.get('/2/member/self').body)
       end
 
+      private
+
+      def meetup_location
+        [raw_info['city'], raw_info['state'], raw_info['country']].reject do |v|
+          !v || v.empty?
+        end.join(', ')
+      end
+
+      def meetup_photo_url
+        raw_info.key?('photo') ? raw_info['photo']['photo_link'] : nil
+      end
     end
   end
 end
